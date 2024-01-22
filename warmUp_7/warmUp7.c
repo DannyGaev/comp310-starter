0f04d821e4063fd0a9cfdea2d98185b4a465d9f9
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2024-01-22 23:04:38 +0000

    Committed warmUp7

---
+++ warmUp_7/warmUp7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int linearSearch(int a[], int num);
+
+int main()
+{
+    int a[] = {1,4,7,9,12};
+    int num = 9;
+
+    int index = linearSearch(a,num);
+    if(index > -1)
+    {
+        printf("Element was found at index %d in the array :3\n",index);
+    }
+    else
+    {
+        printf("Element was not found in the array :(\n");
+    }
+
+}
+
+int linearSearch(int a[], int num)
+{
+    for(int i=0; i<sizeof(&a); i++)
+    {
+        if(a[i]==num)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
