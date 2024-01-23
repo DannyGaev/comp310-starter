e8034cb12e53e8fac031b5c972ea184c9b112903
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2024-01-23 16:27:48 +0000

    Updated main method and added header in warmUp6

--- warmUp_6/warmUp6.c
+++ warmUp_6/warmUp6.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int factorial(int n);
 
-int main()
+int main(int argc, char *argv[])
 {
     int num = 5;
     printf("The factorial of %d is %d\n",num,factorial(num));
+
+    return EXIT_SUCCESS;
 
 }
 
