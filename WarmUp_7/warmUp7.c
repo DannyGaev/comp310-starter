609c1dcd68c9cc9e3848844c5cbffc32a203c7f8
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2024-01-25 01:27:20 +0000

    Changed the folder name from warmUp_7 to WarmUp_7, reuploaded all code

---
+++ WarmUp_7/warmUp7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(int a[], int num);
+
+int main(int argc, char *argv[])
+{
+    int a[] = {1,4,7,9,12};
+    int num = 9;
+
+    int index = linearSearch(a,num);
+    if(index > -1)
+    {
+        printf("Element was found at index %d in the array :3\n",index);
+    }
+    else
+    {
+        printf("Element was not found in the array :(\n");
+    }
+    return EXIT_SUCCESS;
+
+}
+
+int linearSearch(int a[], int num)
+{
+    for(int i=0; i<sizeof(&a); i++)
+    {
+        if(a[i]==num)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
