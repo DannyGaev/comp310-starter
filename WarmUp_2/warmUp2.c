24cbcce99cc5ec5f9a852a1e8d22fc9ab7ac8487
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2024-01-25 01:25:32 +0000

    Changed the folder name from warmUp_2 to warmUp_2, reuploaded all code

---
+++ WarmUp_2/warmUp2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int firstNum;
+    int secondNum;
+
+    printf("Please type the first number: \n");
+    scanf("%d",&firstNum);
+
+    printf("Please type the second number: \n");
+    scanf("%d",&secondNum);
+
+    printf("The sum of your numbers is %d\n",firstNum+secondNum);
+    printf("The product of your numbers is %d\n",firstNum*secondNum);
+    printf("The difference of your numbers is %d\n",firstNum-secondNum);
+    printf("The quotient of your numbers is %d\n",firstNum/secondNum);
+
+    return EXIT_SUCCESS;
+}
