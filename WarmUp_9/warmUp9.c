e52a687daa32f449f72931b6e96a5b2bb9ca62dc
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2024-01-25 01:27:37 +0000

    Changed the folder name from warmUp_9 to WarmUp_9, reuploaded all code

---
+++ WarmUp_9/warmUp9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fptr;
+    fptr = fopen("file.txt","w");
+    fprintf(fptr,"Hello there!");
+    fclose(fptr);
+
+    fptr = fopen("file.txt","r");
+    char fileContents[100];
+    fgets(fileContents,100,fptr);
+    printf("Contents of the file: %s\n",fileContents);
+    fclose(fptr);
+    
+    return EXIT_SUCCESS;
+}
